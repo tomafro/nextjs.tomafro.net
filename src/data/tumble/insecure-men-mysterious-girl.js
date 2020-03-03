acb8c39b4d111eabfb53919ccf9b497f2544cf2e
Tom Ward <tom@popdog.net>
2020-03-03 16:00:23 +0000

    It's working SO WELL

---
+++ src/data/tumble/insecure-men-mysterious-girl.js
@@ -0,0 +1,5 @@
+import { SpotifySong, Lyrics } from "../../shared/tumble";
+
+export const content = (
+  <SpotifySong date="2020-03-02" artist="Insecure Men" title="Mysterious Girl" code="4DZ8R2p8XbV78qwxWhqSv1" />
+)
