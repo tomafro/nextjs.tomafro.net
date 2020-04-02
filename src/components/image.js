453149b952e54090248890c28772a3c03b5d908e
Tom Ward <tom@popdog.net>
2020-04-02 16:42:09 +0100

    Better images

--- src/components/image.js
+++ src/components/image.js
@@ -1,14 +1,18 @@
-const Image = ({ src }) => {
+const Image = ({ src, maxWidth = 720, maxHeight = 720}) => {
+  const full = require("../assets/images/" + src + "?resize")
+  const main = require("../assets/images/" + src + "?resize&sizes[]=360&sizes[]=720&sizes[]=1440&sizes[]=2880")
   const webp = require("../assets/images/" + src + "?webp")
   const lqip = require("../assets/images/" + src + "?lqip")
-  const original = require("../assets/images/" + src)
+
+  const width = (full.width / full.height) * maxHeight
+  const height = (full.height / full.width) * maxWidth
 
   return (
     <>
       <picture>
         <source srcSet={webp} type="image/webp"/>
-        <source srcSet={original}/>
-        <img loading="lazy" src={lqip} width="540" height="648" />
+        <source srcSet={main.srcSet}/>
+        <img loading="lazy" style={{backgroundImage: `url(${lqip})`, backgroundSize: "cover"}} width={width} height={height} />
       </picture>
     </>
   )
--- src/data/tumble/2019-09-04-the-ray-tracer-challenge.js
+++ src/data/tumble/2019-09-04-the-ray-tracer-challenge.js
@@ -3,6 +3,6 @@ import Book from "components/book"
 
 export default (
   <Book title="The Ray Tracer Challenge" author="Jamis Buck" url="https://pragprog.com/book/jbtracer/the-ray-tracer-challenge">
-    <Image src="the-ray-tracer-challenge.png"/>
+    <Image src="the-ray-tracer-challenge.jpg"/>
   </Book>
 )
