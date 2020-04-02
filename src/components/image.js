9043c9303f36eaa9770a475031ae3c2e94e91c14
Tom Ward <tom@popdog.net>
2020-04-02 17:46:02 +0100

    Proper image ratios!

--- src/components/image.js
+++ src/components/image.js
@@ -1,18 +1,31 @@
 const Image = ({ src, maxWidth = 720, maxHeight = 720}) => {
+  console.log(maxWidth, maxHeight)
   const full = require("../assets/images/" + src + "?resize")
   const main = require("../assets/images/" + src + "?resize&sizes[]=360&sizes[]=720&sizes[]=1440&sizes[]=2880")
   const webp = require("../assets/images/" + src + "?webp")
-  const lqip = require("../assets/images/" + src + "?lqip")
+  const fallback = require("../assets/images/" + src + "?lqip")
 
-  const width = (full.width / full.height) * maxHeight
-  const height = (full.height / full.width) * maxWidth
+  var width = full.width
+  var height = full.height
+
+  const ratio = full.width / full.height
+  const maxRatio = maxWidth / maxHeight
+
+  if (ratio < maxRatio && height > maxHeight) {
+    height = maxHeight
+    width = ratio * maxHeight
+  }
+  else if (width > maxWidth) {
+    width = maxWidth
+    height = maxWidth / ratio
+  }
 
   return (
     <>
       <picture>
         <source srcSet={webp} type="image/webp"/>
         <source srcSet={main.srcSet}/>
-        <img loading="lazy" style={{backgroundImage: `url(${lqip})`, backgroundSize: "cover"}} width={width} height={height} />
+        <img loading="lazy" width={width} height={height} style={{backgroundImage: `url(${fallback})`, backgroundSize: "cover"}}  />
       </picture>
     </>
   )
