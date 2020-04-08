3233c6ea51ed47e29dcba2f3d8715519b601cc62
Tom Ward <tom@popdog.net>
2020-04-08 15:22:39 +0100

    Add coronavirus book

---
+++ src/pages/_links/2020-04-07-coronavirus-a-book-for-children.js
@@ -0,0 +1,11 @@
+import { Image } from "components"
+import Book from "components/book"
+
+export default (
+  <Book title="Coronavirus: A Book for Children" url="https://nosycrow.com/blog/released-today-free-information-book-explaining-coronavirus-children-illustrated-gruffalo-illustrator-axel-scheffler/">
+    <Image src="coronavirus-a-book-for-children.png"
+      title="Coronavirus: A Book for Children"
+      alt="Cover of the book 'Coronavirus: A Book for Children' showing illustrations of doctors, children washing hands, video calls with grandparents, and more"
+           maxWidth="360" />
+  </Book>
+)
