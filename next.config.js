91769b8418028640245612940dd2e1deba48692d
Tom Ward <tom@popdog.net>
2020-05-08 11:50:23 +0100

    Use markdown for projects. Yay!

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withOptimizedImages = require('next-optimized-images');
+const withMDX = require('@next/mdx')();
 
-
-module.exports = withOptimizedImages({
+module.exports = withMDX(withOptimizedImages({
   experimental: {
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   }
-})
+}))
--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,12 @@
 import Layout from "components/layout"
+import { Project } from "components"
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 
 class Projects extends React.Component {
   get projects() {
-    return this.props.paths.map(path => require("projects/" + path).default)
+    return this.props.paths.map(path => require("projects/" + path))
   }
 
   render() {
@@ -19,19 +21,34 @@ class Projects extends React.Component {
   }
 
   renderProject(project) {
-    return (
-      <li key={project.props.url}>
-        <span className="fa-li"><FontAwesomeIcon icon={faCodeBranch} /></span>
-        { project }
-      </li>
-    )
+    if (project.default.isMDXComponent) {
+      const MDX = project.default
+      const metadata = project.metadata || {}
+      console.log(metadata.url)
+      return (
+        <li key={metadata.url} >
+          <span className="fa-li"><FontAwesomeIcon icon={faCodeBranch} /></span>
+          <Project title={metadata.title} url={metadata.url}>
+            <MDX />
+          </Project>
+        </li>
+      )
+    }
+    else {
+      return (
+        <li key={project.default.props.url}>
+          <span className="fa-li"><FontAwesomeIcon icon={faCodeBranch} /></span>
+          { project.default }
+        </li>
+      )
+    }
   }
 }
 
 export async function getStaticProps() {
   const loader = await import("support/loader")
   const paths = await loader.findPaths("projects")
-  return { props: {paths: paths.sort() } }
+  return { props: { paths: paths.sort() } }
 }
 
 export default Projects
