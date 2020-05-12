8d91796cc094a3027cddbddb3ac0b73f7ca291d4
Tom Ward <tom@popdog.net>
2020-05-12 12:36:31 +0100

    Cleaner, leaner, components

--- pages/projects.js
+++ pages/projects.js
@@ -1,14 +1,12 @@
-import { Layout, Project } from "components"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Layout, Project, ListWithIcons } from "components"
 
 export * from "page/config"
 
-function renderProject(project) {
+function projectItem(project) {
   return (
-    <li key={project.url} >
-      <span className="fa-li"><FontAwesomeIcon icon="code-branch" /></span>
+    <ListWithIcons.Item key={project.url} icon="code-branch">
       <Project {...project} />
-    </li>
+    </ListWithIcons.Item>
   )
 }
 
@@ -17,9 +15,9 @@ export default ({ paths }) => {
   return (
     <Layout title="Projects">
       <h1>Recent Projects</h1>
-      <ol className="links with-icons">
-        {projects.map(renderProject)}
-      </ol>
+      <ListWithIcons>
+        { projects.map(projectItem) }
+      </ListWithIcons>
     </Layout>
   )
 }
