0abb08e8b4b9d9153af62eafc8fecf9113a4f0f5
Tom Ward <tom@popdog.net>
2020-05-12 09:58:52 +0100

    Less is more

--- pages/projects.js
+++ pages/projects.js
@@ -3,57 +3,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export * from "page/config"
 
-function contentFor(module) {
-  if (module.default.isMDXComponent) {
-    const MDX = module.default
-    return (
-      <>
-        <MDX />
-      </>
-    )
-  }
-  else {
-    return module.default
-  }
+function renderProject(project) {
+  return (
+    <li key={project.url} >
+      <span className="fa-li"><FontAwesomeIcon icon="code-branch" /></span>
+      <Project {...project} />
+    </li>
+  )
 }
 
-function itemFor(module) {
-  return {
-    children: contentFor(module),
-    ...module
-  }
-}
-
-class Projects extends React.Component {
-  get projects() {
-    return require("data").load(this.props.paths)
-  }
-
-  render() {
-    return (
-      <Layout title="Projects">
-        <h1>Recent Projects</h1>
-        <ol className="links with-icons">
-          { this.projects.map(this.renderProject) }
-        </ol>
-      </Layout>
-    )
-  }
-
-  renderProject(project) {
-    const item = itemFor(project)
-
-    return (
-      <li key={item.url} >
-        <span className="fa-li"><FontAwesomeIcon icon="code-branch" /></span>
-        <Project {...item}/>
-      </li>
-    )
-  }
+export default ({ paths }) => {
+  const projects = require("data").load(paths)
+  return (
+    <Layout title="Projects">
+      <h1>Recent Projects</h1>
+      <ol className="links with-icons">
+        {projects.map(renderProject)}
+      </ol>
+    </Layout>
+  )
 }
 
 export async function getStaticProps() {
   return { props: { paths: require("data").projectPaths() } }
 }
-
-export default Projects
