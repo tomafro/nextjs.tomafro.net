09cf28f96fab447b5c8712bbc879121fc97ee8e0
Tom Ward <tom@popdog.net>
2020-05-29 21:25:45 +0100

    Remove console.logs

--- pages/calendars/[name].js
+++ pages/calendars/[name].js
@@ -9,8 +9,6 @@ export async function getStaticProps() {
 }
 
 export async function getStaticPaths() {
-  console.log(process.cwd())
-  console.log(process.cwd() + "/public/tom.ical")
   const fs = require('fs');
   fs.writeFile(process.cwd() + "/public/tom.ical", "Hello There", () => {})
 
