a0ebc0b7f569a008a5b48379baefc2966d9e1cbc
Tom Ward <tom@popdog.net>
2020-05-29 08:51:58 +0100

    Reducto reductum

--- data/links/2019-09-06-beta-apple-music-web.js
+++
@@ -1,9 +0,0 @@
-import { Misc } from "components"
-
-export const url = "https://beta.music.apple.com"
-
-export default (
-  <Misc icon="music">
-    <a href={url}>music web app</a> is unsurprisingly better than iTunes, even as a beta.
-  </Misc>
-)
