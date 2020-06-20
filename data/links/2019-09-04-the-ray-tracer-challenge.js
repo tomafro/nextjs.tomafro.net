d6184f390d1bb6d6c743a0fc35c701baa4baa570
Tom Ward <tom@popdog.net>
2020-06-20 11:42:31 +0100

    An image appears

--- data/links/2019-09-04-the-ray-tracer-challenge.js
+++ data/links/2019-09-04-the-ray-tracer-challenge.js
@@ -1,5 +1,6 @@
 import Book from "components/book"
 
+
 export default (
   <Book title="The Ray Tracer Challenge" author="Jamis Buck" url="https://pragprog.com/book/jbtracer/the-ray-tracer-challenge">
     <picture image="rayTracerChallenge">
--- next.config.js
+++ next.config.js
@@ -4,6 +4,59 @@ const sharp = require('sharp')
 const lqip = require('lqip')
 const fs = require('fs')
 
+const destination = 'images'
+
+const createVariants = async function(inputPath) {
+  const inputImage = sharp(inputPath)
+  const inputImageMetadata = await inputImage.metadata()
+  const rx = /(?<name>[^\/]+)\.(?<ext>jpg|png)/
+  const pathInfo = rx.exec(inputPath).groups
+
+  console.log(inputImageMetadata)
+
+  const image = {
+    width: inputImageMetadata.width,
+    height: inputImageMetadata.height,
+    lqip: await lqip.base64(inputPath)
+  }
+
+  const sizes = [0.6, 0.36, 0.22, 0.13]
+
+  const pngVariants = sizes.map(size => {
+    const width = Math.round(image.width * size)
+    const path = `${destination}/${pathInfo.name}.${width}.png`
+    sharp(inputPath).resize({ width: width }).png({ palette: true }).toFile(`public/${path}`)
+    return { type: "image/png", width: width, path: path }
+  })
+
+  const webpVariants = sizes.map(size => {
+    const width = Math.round(image.width * size)
+    const path = `${destination}/${pathInfo.name}.${width}.webp`
+    sharp(inputPath).resize({ width: width }).webp({ smartSubsample: true, reductionEffort: 6 }).toFile(`public/${path}`)
+    return { type: "image/webp", width: width, path: path }
+  })
+
+  const variants = webpVariants.concat(pngVariants).map(({ type, width, path }) => {
+    return `{ type: "${type}", width: ${width}, image: "/${path}"}`
+  })
+
+  console.log(variants)
+
+  await sharp(inputPath).png({ palette: true }).toFile(`public/${destination}/${pathInfo.name}.png`)
+  await sharp(inputPath).webp({ smartSubsample: true, reductionEffort: 6 }).toFile(`public/${destination}/${pathInfo.name}.webp`)
+
+  fs.writeFileSync(`data/images/${pathInfo.name}.js`,
+    `export default {
+    width: ${image.width},
+    height: ${image.height},
+    lqip: "${image.lqip}",
+    variants: [
+      ${variants.join(",\n    ")}
+    ]
+  }
+  `)
+}
+
 module.exports = withMDX({
   experimental: {
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
@@ -33,59 +86,7 @@ module.exports = withMDX({
 
     config.plugins.push(new PrebuildPlugin({
       build: async function(compiler, compilation, matchedFiles) {
-        const destination = 'build/images'
-        fs.mkdirSync(destination, { recursive: true })
-
-        const inputPath = matchedFiles[0]
-        const inputImage = sharp(inputPath)
-        const inputImageMetadata = await inputImage.metadata()
-        const rx = /(?<name>[^\/]+)\.(?<ext>jpg|png)/
-        const pathInfo = rx.exec(inputPath).groups
-
-
-        console.log(inputImageMetadata)
-
-        const image = {
-          width: inputImageMetadata.width,
-          height: inputImageMetadata.height,
-          lqip: await lqip.base64(inputPath)
-        }
-
-        const sizes = [0.6, 0.36, 0.22, 0.13]
-
-        const pngVariants = sizes.map(size => {
-          const width = Math.round(image.width * size)
-          const path = `${destination}/${pathInfo.name}.${width}.png`
-          sharp(inputPath).resize({ width: width }).png({ palette: true }).toFile(path)
-          return { type: "image/png", width: width, path: path }
-        })
-
-        const webpVariants = sizes.map(size => {
-          const width = Math.round(image.width * size)
-          const path = `${destination}/${pathInfo.name}.${width}.webp`
-          sharp(inputPath).resize({ width: width }).webp({ smartSubsample: true, reductionEffort: 6 }).toFile(path)
-          return { type: "image/webp", width: width, path: path }
-        })
-
-        const variants = webpVariants.concat(pngVariants).map(({type, width, path}) => {
-          return `{ type: "${type}", width: ${width}, image: require("${path}")}`
-        })
-
-        console.log(variants)
-
-        await sharp(inputPath).png({ palette: true}).toFile (`${destination}/${pathInfo.name}.png`)
-        await sharp(inputPath).webp({ smartSubsample: true, reductionEffort: 6 }).toFile(`${destination}/${pathInfo.name}.webp`)
-
-        fs.writeFileSync(`${destination}/${pathInfo.name}.js`,
-`export default {
-  width: ${image.width},
-  height: ${image.height},
-  lqip: "${image.lqip}",
-  variants: [
-    ${variants.join(",\n    ")}
-  ]
-}
-`)
+        matchedFiles.forEach(createVariants)
       },
       watch: (compiler, compilation, changedFile) => {
         // function that runs each time webpack rebuilds in dev mode. if `files.pattern` is provided,
