1f8d9ba22c3a75be1a8f7b02e5bc6e2f8ecd5a29
Tom Ward <tom@popdog.net>
2020-06-23 21:03:07 +0100

    Clean up link icons and markup

--- data/links/2019-08-24-marina-hyde-on-prince-andrew.js
+++ data/links/2019-08-24-marina-hyde-on-prince-andrew.js
@@ -2,7 +2,7 @@ import { Quote } from "components"
 
 export default (
   <Quote author="Marina Hyde" url="https://www.theguardian.com/commentisfree/2019/aug/23/prince-andrew-appalled-epstein-royal-wedding">
-    They suggest the kind of man – and we’ve all met them – who has a two - tier view of the female sex.There is a world for their daughters, hopefully insulated from men like their friend Jeffrey, and then there is another world for the girls who service their friend Jeffrey.
+    They suggest the kind of man – and we’ve all met them – who has a two - tier view of the female sex. There is a world for their daughters, hopefully insulated from men like their friend Jeffrey, and then there is another world for the girls who service their friend Jeffrey.
     <cite><a href="https://www.theguardian.com/commentisfree/2019/aug/23/prince-andrew-appalled-epstein-royal-wedding">Marina Hyde on Prince Andrew and Jeffrey Epstein</a></cite>
   </Quote>
 )
