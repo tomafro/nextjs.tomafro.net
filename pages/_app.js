3091ad909197d046f0029e54fa54eb05fb21f872
Tom Ward <tom@popdog.net>
2020-12-21 20:44:41 +0000

    Fix imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import "../assets/css/style.scss"
-import "@tomafro/fontawesome-svg-core/styles.css"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 
-import { library } from '@tomafro/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@tomafro/pro-solid-svg-icons'
-import { fab } from '@tomafro/free-brands-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, fab)
 
